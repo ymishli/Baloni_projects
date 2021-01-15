339dd4e30deb36b71a77caf473369dbd1b2dc577
Yakir <yakir.mishli@gmail.com>
2021-01-15 08:42:37 -0800

    section a,b of Q1 done. section c won't work unless we comment set_field(c) line.

--- Ultrasound_field2/Q1.m
+++ Ultrasound_field2/Q1.m
@@ -4,11 +4,13 @@ clc; close all;clear all;
 %addpath('C:\Users\Rony\Desktop\Ultrasound\Field2\Field_II_ver_3_22_windows');
 field_init(0)
 
+% Control the plots plot_on = [sec_a sec_b sec_c sec_d sec_e]
+plot_on = [0 0 1 0 0];
+
 % Generate the transducer aperture for send and receive
 f0          = 2.5e6;            %  Transducer center frequency [Hz]
 fs          = 100e6;            %  Sampling frequency [Hz]
 c           = 1490;             %  Speed of sound [m/s]
-% c           = 1540;             %  Speed of sound [m/s]
 lambda      = c/f0;             %  Wavelength [m]
 width       = 18.5/1000;        %  Width of element
 height      = 13/1000;          % Height of element [m]
@@ -34,59 +36,72 @@ excitaion = sin(2*pi*f0*t);
 xdc_excitation(tx,excitaion());
 
 %1.a
-show_xdc_geir(tx,1);
-axis equal;
-view(3);
-figure;
-plot(t,excitaion);
-title('excitation');
-xlabel('t [sec]');
-ylabel('V');
+if plot_on(1) == 1    
+    figure('Name','Q1 - Section 1 - Transducer Geometry');
+    show_xdc_geir(tx,1);    
+    axis equal;
+    view(3);
+    title('Q1 - Section 1 - Transducer Geometry');
+    figure('Name','Q1 - Section 1 - Excitation signal');
+    plot(t,excitaion);
+    title('Q1 - Section 1 - Excitation signal');
+    xlabel('t [sec]');
+    ylabel('V');
+end
 
 %1.b
 Bw = 0.6;
 t_h = (-2/f0:1/fs:2/f0);
-impulse_response = gauspuls(t_h,f0,Bw);
-impulse_response = impulse_response.* sin(2*pi*f0*t_h);
-xdc_impulse(tx,impulse_response);
-figure;
-plot(t_h,impulse_response);
-figure;
-freqz(impulse_response);
+impulse_response = gauspuls(t_h,f0,Bw);    
+xdc_impulse(tx,impulse_response);    
+if plot_on(2) == 1    
+    figure('Name','Q1 - Section 2 - Impulse Response in Time');
+    plot(t_h,impulse_response);    
+    title('Q1 - Section 2 - Impulse Response in Time');
+    xlabel('t [sec]');
+    ylabel('V');
+    figure('Name','Q1 - Section 2 - Impulse Response in Frequncy domain');    
+    freqz(impulse_response);
+    title('Q1 - Section 2 - Impulse Response in Frequncy domain');
+end
 
 %1.c
-[h, start_time] = calc_hp (tx,[0,0,40/1000]);
-figure;
-plot(h);
+if plot_on(3) == 1
+    [h, start_time] = calc_hp (tx,[0,0,40/1000]);
+    figure;
+    plot(h);
+end
+
 
 %1.d
-[x,y,z]=meshgrid(linspace(-10,10,100)/1000,0,linspace(10,80,100)/1000);
-points=[x(:) y(:) z(:)];
-Im_size=[length(x),1,length(z)];
-[hp,start_t]=calc_hp(tx,points);
-[m,n]=size(hp);
+if plot_on(4) == 1
+    [x,y,z]=meshgrid(linspace(-10,10,100)/1000,0,linspace(10,80,100)/1000);
+    points=[x(:) y(:) z(:)];
+    Im_size=[length(x),1,length(z)];
+    [hp,start_t]=calc_hp(tx,points);
+    [m,n]=size(hp);
 
-% With 'Norm' on each impulse response
-for i=1:n
-  P1(i) = norm(hp(:,i));
+    % With 'Norm' on each impulse response
+    for i=1:n
+      P1(i) = norm(hp(:,i));
+    end
+    P1=reshape(P1,[Im_size(1),Im_size(3)]);
+    P1=rot90(P1,1);
+    Result=flipud(P1);
+    Result_norm=Result-min(min(Result));
+    Result_norm=Result_norm/max(max(Result_norm));
+    figure;
+    subplot(1,2,1);
+    imagesc(1000*x(:),1000*z(:),Result_norm);
+    colormap(hot)
+    title('Transmit Field Example');
+    xlabel('X[mm]');ylabel('Z[mm]');
+    % Convert Intensity results to dB
+    %  Result_dB=Convert2dB(Result);
+    %  subplot(1,2,2);
+    %  imagesc(1000*x(:),1000*z(:),Result_dB);
+    %  colormap(hot);
+    %  title('Transmit Field Example dB');
+    %  xlabel('X[mm]');ylabel('Z[mm]');
+    %  colorbar
 end
-P1=reshape(P1,[Im_size(1),Im_size(3)]);
-P1=rot90(P1,1);
-Result=flipud(P1);
-Result_norm=Result-min(min(Result));
-Result_norm=Result_norm/max(max(Result_norm));
-figure;
-subplot(1,2,1);
-imagesc(1000*x(:),1000*z(:),Result_norm);
-colormap(hot)
-title('Transmit Field Example');
-xlabel('X[mm]');ylabel('Z[mm]');
-
-% Convert Intensity results to dB
-%  Result_dB=Convert2dB(Result);
-%  subplot(1,2,2);
-%  imagesc(1000*x(:),1000*z(:),Result_dB);
-%  colormap(hot);
-%  title('Transmit Field Example dB');
-%  xlabel('X[mm]');ylabel('Z[mm]');
-%  colorbar
